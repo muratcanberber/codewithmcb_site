9d58478505af9c9df94120f0ef81a9561193d68b
Murat Can BERBER <muratcanberber@gmail.com>
2025-04-11 17:31:22 +0300

    12313

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,5 +1,10 @@
 // src/App.tsx
 
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
+import honoLogo from "./assets/hono.svg";
 import "./App.css";
 
 function App() {
@@ -8,8 +13,39 @@ function App() {
 
   return (
     <>
-<h1>Vite + React + Hono + Cloudflare</h1>
+      <div>
 
+      </div>
+      <h1>Vite + React + Hono + Cloudflare</h1>
+      <div className="card">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          aria-label="increment"
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <div className="card">
+        <button
+          onClick={() => {
+            fetch("/api/")
+              .then((res) => res.json() as Promise<{ name: string }>)
+              .then((data) => setName(data.name));
+          }}
+          aria-label="get name"
+        >
+          Name from API is: {name}
+        </button>
+        TEST ME
+        <p>
+          Edit <code>worker/index.ts</code> to change the name
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the logos to learn more</p>
+    </>
   );
 }
 
